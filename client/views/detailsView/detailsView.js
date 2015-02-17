ff7af06cd1ce9691b0a0e1026cf24da48c75ec53
Nick <nick.bucheleres@gmail.com>
2015-02-16 16:51:54 -0800

    got parentId to carry through to detailsView, need to persist each lists details

--- client/views/detailsView/detailsView.js
+++ client/views/detailsView/detailsView.js
@@ -2,7 +2,34 @@
 
 Router.route('detailsView', {
 
-	path: '/details',
+	path: '/details/:_id',
 	template: 'detailsViewTemplate',
-	loadingTemplate: 'loading'
+	loadingTemplate: 'loading',
+	waitOn: function(){
+		return Meteor.subscribe('lists');
+	},
+
+	data: function(){
+		return listCollection.findOne({_id: this.params._id});
+	}
+});
+
+Template.detailsViewTemplate.helpers({
+
+	detailsCollection: function(){
+		return detailCollection.find();
+	}
+});
+
+Template.detailsViewTemplate.events({
+	'submit form': function(ev){
+		ev.preventDefault();
+
+		var newDetail = {
+			detail: $(ev.target).find('[name = detail]').val(),
+			parentId: $(ev.target).find('[name = parentId]').val()
+		}
+
+		Meteor.call('newDetail', newDetail);
+	}
 });
