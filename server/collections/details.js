33cd2150dcfd876288e7ddbc8d5a563f63debc59
Nick <nick.bucheleres@gmail.com>
2015-02-18 15:20:39 -0800

    clean commit to pull to air

--- server/collections/details.js
+++ server/collections/details.js
@@ -1,7 +1,10 @@
 //server/collections/details.js
 
 Meteor.publish('details', function(parentId){
-	if(this.userId){
-		return detailsCollection.find({parentId: parentId});
+	if(!this.userId){
+		return this.ready();
 	}
+		
+		return detailsCollection.find({parentId: parentId});
+	
 });
--- server/collections/lists.js
+++ server/collections/lists.js
@@ -1,7 +1,9 @@
 //server/collections/lists.js
 
 Meteor.publish('lists', function(){
-	if(this.userId){
-		return listCollection.find({owner: this.userId});
+	if(!this.userId){
+		return this.ready();
 	}
+
+	return listCollection.find({owner: this.userId});
 });
