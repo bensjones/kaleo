10ee189b0d1e36def01808672b9ae526785c0959
Nick <nick.bucheleres@gmail.com>
2015-04-01 15:43:54 -0700

    clean commit

--- server/collections/lists.js
+++ server/collections/lists.js
@@ -7,13 +7,3 @@ Meteor.publish('lists', function(){
 		return listCollection.find({owner: this.userId});
 	}
 });
-
-Meteor.publish('users', function(){
-	// use this.added to inform subscriber that new doc as been added
-	// this.added(collection, id, fields)
-
-	// var selector = {};
- 	//  var options = {fields: {username: 1}};
- 	//  return Meteor.users.find(selector, options).fetch();
-	return Meteor.users.find();
-});
