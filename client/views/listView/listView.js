f97cdd629bf0bbc8dc4ced0eca1785b54b6e43ae
Nick <nick.bucheleres@gmail.com>
2015-04-06 10:53:26 -0700

    click delete-button shades out, clicking again deletes detail; need to make delete button not affected by shading

--- client/views/listView/listView.js
+++ client/views/listView/listView.js
@@ -95,7 +95,7 @@ Template.listViewTemplate.events({
 
 	'click .select_to_share': function(ev){
 		ev.preventDefault();
-		shared_user_input = document.getElementById('shared_user_field');
+		var shared_user_input = document.getElementById('shared_user_field');
 		shared_user_input.value = this._id;
 	}
 })
