59a650257214dbb15873df5b9f140ceebfad5c81
Nick <nick.bucheleres@gmail.com>
2015-04-14 10:32:14 -0700

    create and read cookie functions in place

--- client/views/detailsView/detailsView.js
+++ client/views/detailsView/detailsView.js
@@ -56,13 +56,15 @@ Template.detailsViewTemplate.events({
 	},
 
 	'click .detailCheckbox': function(ev){
-		var detail = $(ev.target).parent();
+		// var detail = $(ev.target).parent();
 
-		if(!detail.hasClass('toggle')){
-			detail.addClass('toggle');	
-		} else {
-			detail.removeClass('toggle');
-		}
+		// if(!detail.hasClass('toggle')){
+		// 	detail.addClass('toggle');	
+		// } else {
+		// 	detail.removeClass('toggle');
+		// }
+
+		createCookie('')
 		
 	},
 
@@ -75,3 +77,33 @@ Template.detailsViewTemplate.events({
 		}
 	}
 });
+
+// ~~~~COOKIES FOR DETAIL CHECKBOX OPACITY~~~~
+
+// function that creates the cookie
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+// function that reads the cookie
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
